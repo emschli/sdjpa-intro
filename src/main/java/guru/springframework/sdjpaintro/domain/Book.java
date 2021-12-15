070d110f73c25ceb74b0caa371070b9c635a4214
Mirjam Schliephack <mschliephack@novomind.com>
2021-12-15 12:50:17 +0100

    Section 2 done

--- src/main/java/guru/springframework/sdjpaintro/domain/Book.java
+++ src/main/java/guru/springframework/sdjpaintro/domain/Book.java
@@ -11,17 +11,17 @@ import javax.persistence.Id;
 @Entity
 public class Book {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-  private String title;
-  private String isbn;
-  private String publisher;
+    private String title;
+    private String isbn;
+    private String publisher;
 
-  public Book(){
+    public Book() {
 
-  }
+    }
 
     public Book(String title, String isbn, String publisher) {
         this.title = title;
@@ -29,35 +29,50 @@ public class Book {
         this.publisher = publisher;
     }
 
-  public Long getId() {
-    return id;
-  }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+        Book book = (Book) o;
 
-  public String getTitle() {
-    return title;
-  }
+        return id != null ? id.equals(book.id) : book.id == null;
+    }
 
-  public void setTitle(String title) {
-    this.title = title;
-  }
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 
-  public String getIsbn() {
-    return isbn;
-  }
+    public Long getId() {
+        return id;
+    }
 
-  public void setIsbn(String isbn) {
-    this.isbn = isbn;
-  }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-  public String getPublisher() {
-    return publisher;
-  }
+    public String getTitle() {
+        return title;
+    }
 
-  public void setPublisher(String publisher) {
-    this.publisher = publisher;
-  }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
 }
